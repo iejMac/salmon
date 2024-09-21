50df00ca17e8e71e7f58086bf336b39ae75abb56
iejMac <kilianmaciej6@gmail.com>
2024-09-21 09:07:54 +0000

    change to bucket_cap_mb-based assignment

--- salmon/dist/reducer.py
+++ salmon/dist/reducer.py
@@ -10,12 +10,12 @@ class Reducer:
     It does this by creating gradient buffers for the flattened, concatenated gradients of each "bucket"
     and calling asynchronous all_reduce on those vs. the actual param.grad
     """
-    def __init__(self, named_parameters, world_size, num_buckets=1):
-        self.num_buckets = num_buckets
+    def __init__(self, named_parameters, world_size, bucket_cap_mb=25.0):
+        self.bucket_cap_mb = bucket_cap_mb
         self.world_size = world_size
 
         self._init_params(named_parameters)
-        self._init_buckets(num_buckets)
+        self._init_buckets(bucket_cap_mb)
 
     def _init_params(self, named_parameters):
         self.params = {}  # access to param and grad
@@ -28,21 +28,31 @@ class Reducer:
             post_grad_hook = lambda g, k=k: self._post_grad_hook(k)
             p.register_post_accumulate_grad_hook(post_grad_hook)
 
-    def _init_buckets(self, num_buckets):
+    def _init_buckets(self, bucket_cap_mb):
         self.b_params = []  # which params are in the bucket
         self.b_work = []  # async work object for reducing buffer
         self.grad_buffers = []  # buffer tensors
 
-        p_names = list(self.p_ready.keys())
-        p_names.reverse()  # reverse order since thats usually order of grad computation
-        # TODO: do based on p_mb, for now just uniformly
-        # p_mb = p.data.numel() * p.data.element_size() / (1024 ** 2)  # parameter size in MB
-        bucket_size = len(p_names) // num_buckets
-        self.b_params = [p_names[i * bucket_size:(i + 1) * bucket_size] for i in range(num_buckets)]
-        for i, b_params in enumerate(self.b_params):
+        # assign params to buckets
+        self.b_params = []
+        b_i_mb, cur_bucket = 0.0, []
+        for k in list(self.p_ready.keys())[::-1]:  # reverse order since thats usually order of grad computation
+            b_i_mb += self.params[k].data.numel() * self.params[k].data.element_size() / (1024 ** 2)  # parameter size in MB
+            cur_bucket.append(k)
+            self.p_bucket[k] = len(self.b_params)  # param k belongs to bucket i
+            if b_i_mb > bucket_cap_mb:  # start new bucket
+                self.b_params.append(cur_bucket)
+                b_i_mb, cur_bucket = 0.0, []
+                continue
+        if len(cur_bucket) > 0:  # remainder in new bucket
+            self.b_params.append(cur_bucket)
+
+        print(self.b_params)
+
+        # create buffers
+        for b_ps in self.b_params:
             buffer_size = 0
-            for k in b_params:
-                self.p_bucket[k] = i  # param k belongs to bucket i
+            for k in b_ps:
                 n_weights = self.params[k].numel()
                 start, end = buffer_size, buffer_size + n_weights
                 self.p_gb_slice[k] = slice(start, end)  # grad_buffer[buf_size:buf_size+n_w] = p.grad
@@ -51,8 +61,10 @@ class Reducer:
             self.grad_buffers.append(torch.zeros((buffer_size,), dtype=dtype, device=device))
         self.b_work = []
 
+        quit()
+
     def _post_grad_hook(self, k):
-        self.p_ready.update({k: True})  # set param
+        self.p_ready.update({k: True})  # ready up param
 
         b_i = self.p_bucket[k]
         p_sl = self.p_gb_slice[k]
@@ -78,5 +90,5 @@ class Reducer:
         self.b_work = []
         for k in self.p_ready.keys():
             self.p_ready[k] = False
-        for gb in self.grad_buffers: # NOTE: not needed since all ops are memcpy
+        for gb in self.grad_buffers: # NOTE: not needed since all ops are memcpy, do we need it?
             gb.zero_()
